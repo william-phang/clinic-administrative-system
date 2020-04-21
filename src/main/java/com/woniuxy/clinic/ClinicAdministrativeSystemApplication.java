ce3e0539cdb51f6348e08a5a865d32876409ae23
william <1318017435@qq.com>
2020-04-21 15:29:40 +0800

    init project

---
+++ src/main/java/com/woniuxy/clinic/ClinicAdministrativeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.woniuxy.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicAdministrativeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicAdministrativeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woniuxy/clinic/ClinicAdministrativeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woniuxy.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAdministrativeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
