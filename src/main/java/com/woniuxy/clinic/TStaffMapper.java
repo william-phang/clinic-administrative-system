bf5169a187172e9279d4c9909d3be37149d2c166
fjy147258 <61533946+fjy147258@users.noreply.github.com>
2020-04-28 00:06:52 +0800

    Add files via upload

---
+++ src/main/java/com/woniuxy/clinic/TStaffMapper.java
@@ -0,0 +1,32 @@
+package com.woniuxy.clinic.mapper;
+
+import com.woniuxy.clinic.entity.TStaff;
+import com.woniuxy.clinic.entity.TStaffExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TStaffMapper {
+    int countByExample(TStaffExample example);
+
+    int deleteByExample(TStaffExample example);
+
+    int deleteByPrimaryKey(Integer staffId);
+
+    int insert(TStaff record);
+
+    int insertSelective(TStaff record);
+
+    List<TStaff> selectByExample(TStaffExample example);
+
+    TStaff selectByPrimaryKey(Integer staffId);
+    
+    TStaff selectByname(String staff_name);
+
+    int updateByExampleSelective(@Param("record") TStaff record, @Param("example") TStaffExample example);
+
+    int updateByExample(@Param("record") TStaff record, @Param("example") TStaffExample example);
+
+    int updateByPrimaryKeySelective(TStaff record);
+
+    int updateByPrimaryKey(TStaff record);
+}
---
+++ src/main/java/com/woniuxy/clinic/TUserMapper.java
@@ -0,0 +1,32 @@
+package com.woniuxy.clinic.mapper;
+
+import com.woniuxy.clinic.entity.TUser;
+import com.woniuxy.clinic.entity.TUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TUserMapper {
+    int countByExample(TUserExample example);
+
+    int deleteByExample(TUserExample example);
+
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(TUser record);
+
+    int insertSelective(TUser record);
+
+    List<TUser> selectByExample(TUserExample example);
+
+    TUser selectByPrimaryKey(Integer userId);
+    
+    TUser selectByuser_account(String user_account);
+
+    int updateByExampleSelective(@Param("record") TUser record, @Param("example") TUserExample example);
+
+    int updateByExample(@Param("record") TUser record, @Param("example") TUserExample example);
+
+    int updateByPrimaryKeySelective(TUser record);
+
+    int updateByPrimaryKey(TUser record);
+}
